2fac950844a9ab84b34cb2ba88022c386e09a1ff
nudnov1 <81551670+nudnov1@users.noreply.github.com>
2021-04-26 20:24:53 +0300

    Create PowerShell_ Restore Points.ps1

---
+++ PowerShell_ Restore Points.ps1
@@ -0,0 +1,16 @@
+﻿# Enable-ComputerRestore -Drive "c:\"
+Get-ComputerRestorePoint
+# Checkpoint-Computer -Description "Restore_Point_created_23082020" -RestorePointType MODIFY_SETTINGS
+Get-ComputerRestorePoint
+
+
+# Restore-Coomputer -Restore 2
+# Disable-ComputerRestore "c:\"
+
+
+
+Enter-PSSession -ComputerName desktop
+    Get-ComputerRestorePoint
+    exit
+#
+Enter-PSSession -ComputerName
