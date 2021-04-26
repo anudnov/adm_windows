b54184a793321ec1959a79ad2fc4e06148b96b56
nudnov1 <81551670+nudnov1@users.noreply.github.com>
2021-04-26 20:24:26 +0300

    Create PwerShell_ Удаляем файлы старше XXX.ps1

---
+++ PwerShell_ Удаляем файлы старше XXX.ps1
@@ -0,0 +1,4 @@
+﻿#Count files in folder
+(Get-ChildItem | Measure-Object).Count
+# Delete files in c:\temp, older then 5 days
+Get-ChildItem "C:\temp" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-5))}| Remove-Item
