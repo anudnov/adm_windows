b64a4a202826fb0e5d046852786ec681886cf886
Aleksei <al@nudnov.net>
2021-11-10 13:15:03 +0200

    Create PowerShell_ Get Folder Sizes on Disk in Windows.ps1

---
+++ PowerShell_ Get Folder Sizes on Disk in Windows.ps1
@@ -0,0 +1,1 @@
+gci -force ‘D:\video’-ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {$len = 0gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }$_.fullname, ‘{0:N2} GB’ -f ($len / 1Gb)}
