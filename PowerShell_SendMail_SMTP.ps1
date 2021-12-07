0bd9126f420d1321575449291bc612416ddea340
Aleksei <al@nudnov.net>
2021-12-07 12:39:47 +0200

    Update PowerShell_SendMail_SMTP.ps1

--- PowerShell_SendMail_SMTP.ps1
+++ PowerShell_SendMail_SMTP.ps1
@@ -1,1 +1,9 @@
-Send-MailMessage -To “alert@nudnov.net” -From “al@livedns.co.il”  -Subject “Test message subject” -Body “Some important plain text!” -SmtpServer “outgoing.livedns.co.il” -Port 25
+Send-MailMessage -To “mail@domain.name” -From “mail@domain.name”  -Subject “Test message subject” -Body “Some important plain text!” -SmtpServer “outgoing.livedns.co.il” -Port 25
+#
+#
+# Attacement:
+
+Send-MailMessage -To “mail@domain.name” -From “mail@domain.name”  -Subject “Test message subject” -Body “Some important plain text!” -Attachments c:\scripts\data.csv -SmtpServer “outgoing.livedns.co.il” -Port 25
+Send-MailMessage -To “mail@domain.name” -From “mail@domain.name”  -Subject “Test message subject” -Body “Some important plain text!” -Attachments .\data.csv -SmtpServer “outgoing.livedns.co.il” -Port 25
+#.\data.csv = C:\Users\user_name\data.csv
+
