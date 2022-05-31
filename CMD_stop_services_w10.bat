d9b66e84bc01b71e4988f3caa596f3e08710ecca
Aleksei <al@nudnov.net>
2022-05-31 15:49:50 +0300

    Update CMD_stop_services_w10.bat

--- CMD_stop_services_w10.bat
+++ CMD_stop_services_w10.bat
@@ -1,1 +1,15 @@
+ECHO OFF
+ECHO Step 1 - Stopping services.
+net stop IISADMIN /y
+pause 
 
+ECHO Step 2 - Disable the services.
+sc config IISADMIN start=disabled
+
+pause
+
+ECHO Step 3 - Show services status
+ECHO Service: iisadmin 
+sc query "iisadmin" | find "RUNNING" 
+sc query "iisadmin" | find "STOPPED"
+pause
