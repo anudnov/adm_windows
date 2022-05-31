5ac89d0bd4d622988cdf6f7a67dd5efd3b9042c0
Aleksei <al@nudnov.net>
2022-05-31 15:57:13 +0300

    Update CMD_stop_services_w10.bat

--- CMD_stop_services_w10.bat
+++ CMD_stop_services_w10.bat
@@ -1,15 +1,15 @@
 ECHO OFF
 ECHO Step 1 - Stopping services.
-net stop "FoxitReaderUpdateService" /y
+net stop "Spooler" /y
 pause 
 
 ECHO Step 2 - Disable the services.
-sc config "FoxitReaderUpdateService" start=disabled
+sc config "Spooler" start=disabled
 
 pause
 
 ECHO Step 3 - Show services status
 ECHO Service: iisadmin 
-sc query "FoxitReaderUpdateService" | find "RUNNING" 
-sc query "FoxitReaderUpdateService" | find "STOPPED"
+sc query "Spooler" | find "RUNNING" 
+sc query "Spooler" | find "STOPPED"
 pause
