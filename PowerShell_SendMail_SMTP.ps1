1ea4e7f41145b43433eddcc89499d128d063c9f3
Aleksei <al@nudnov.net>
2022-11-16 10:05:41 +0200

    Update PowerShell_SendMail_SMTP.ps1

--- PowerShell_SendMail_SMTP.ps1
+++ PowerShell_SendMail_SMTP.ps1
@@ -7,3 +7,21 @@ Send-MailMessage -To “mail@domain.name” -From “mail@domain.name”  -Subje
 Send-MailMessage -To “mail@domain.name” -From “mail@domain.name”  -Subject “Test message subject” -Body “Some important plain text!” -Attachments .\data.csv -SmtpServer “IP_ADDRESS” -Port 25
 #.\data.csv = C:\Users\user_name\data.csv
 
+
+
+# Send email SMTP with PowerShell
+# test send an email SMTP with an account to authenticate the mail server.
+<#
+$username = 'usertest'
+$password = 'Passtest123'
+$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+Send-MailMessage -Credential $mycreds -SmtpServer retail.smtp.com -Port 25 -From usertest@devopslite.com -To ghost@gmail.com -Subject test -Body test
+#>
+<#
+$username = 'usertest'
+$password = 'Passtest123'
+$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+Send-MailMessage -Credential $mycreds -SmtpServer retail.smtp.com -Port 587 -UseSsl -From usertest@devopslite.com -To ghost@gmail.com -Subject test -Body test
+#>
