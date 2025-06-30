3a4967eb377cb1abf707af6489e85bfd32923472
Aleksei <al@nudnov.net>
2025-06-30 07:48:08 +0300

    Create get_vm_&_disk_info.ps1

---
+++ hyper-v/get_vm_&_disk.ps1
@@ -0,0 +1,12 @@
+Get-VM -ComputerName hyperv51 | ForEach-Object {
+    $vm = $_
+    Get-VMHardDiskDrive -VMName $vm.Name -ComputerName hyperv51 | ForEach-Object {
+        $vhd = Get-VHD -Path $_.Path -ComputerName hyperv51
+        [PSCustomObject]@{
+            VMName     = $vm.Name
+            State      = $vm.State
+            DiskPath   = $vhd.Path
+            DiskSizeGB = [math]::Round($vhd.Size / 1GB, 2)
+        }
+    }
+}
